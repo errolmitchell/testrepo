71afa15ccc7c0be63db7f8db242657b79f83c955
Errol Mitchell <errolbmitchell@gmail.com>
2022-09-12 06:05:35 -0400

    files added for test

---
+++ Repo Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Repo_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Repo");
+            Console.ReadKey();
+            Console.WriteLine("setting up new repoistory on command line");
+            Console.ReadKey();
+            Console.WriteLine("Next learn to pull");
+
+        }
+    }
+}
